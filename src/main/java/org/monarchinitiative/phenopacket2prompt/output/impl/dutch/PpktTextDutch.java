ec78c03f72014e61228cbee484a977a2d6243e45
Kyran Wissink <kyran@wissies.nl>
2024-05-16 12:55:51 +0200

    Changed the GPT text to the new method

--- src/main/java/org/monarchinitiative/phenopacket2prompt/output/impl/dutch/PpktTextDutch.java
+++ src/main/java/org/monarchinitiative/phenopacket2prompt/output/impl/dutch/PpktTextDutch.java
@@ -7,14 +7,15 @@ public class PpktTextDutch implements PhenopacketTextGenerator {
     @Override
     public String QUERY_HEADER() {
         return  """
-Ik doe een experiment met een klinisch verslag om te zien hoe jullie diagnoses zich verhouden tot die van menselijke experts. Ik geef je een deel van een medisch geval. Je probeert geen patiënten te behandelen. In dit geval ben je "Dr. GPT-4", een AI-taalmodel dat een diagnose stelt. Hier zijn enkele richtlijnen. Ten eerste is er één definitieve diagnose en dat is een diagnose waarvan bekend is dat die bij mensen bestaat. De diagnose wordt bijna altijd bevestigd door één of andere genetische test, maar in zeldzame gevallen waarin zo'n test niet bestaat voor een diagnose, kan de diagnose in plaats daarvan worden gesteld op basis van gevalideerde klinische criteria of heel zelden gewoon worden bevestigd door de mening van een expert. Nadat je de casus hebt gelezen, wil ik dat je een differentiaaldiagnose stelt met een lijst van mogelijke diagnoses gerangschikt naar waarschijnlijkheid, te beginnen met de meest waarschijnlijke kandidaat. Elke kandidaat moet gespecificeerd worden met de OMIM identifier en de naam van de ziekte. Bijvoorbeeld, als de eerste kandidaat het Branchiooculofaciaal syndroom is en de tweede Cystic fibrosis, geef dan dit:
+Ik voer een experiment uit op basis van een klinisch casusrapport om te zien hoe jouw diagnoses zich verhouden tot die van menselijke experts. Ik ga je een deel van een medische casus geven. Je probeert geen patiënten te behandelen. In dit geval ben je “Dr. GPT-4”, een AI-taalmodel dat een diagnose stelt. Hier zijn enkele richtlijnen. Ten eerste bestaat er één definitieve diagnose, en het is een diagnose waarvan tegenwoordig bekend is dat deze ook bij mensen voorkomt. De diagnose wordt bijna altijd bevestigd door een soort genetische test, hoewel in zeldzame gevallen, wanneer een dergelijke test niet bestaat voor een diagnose, de diagnose in plaats daarvan kan worden gesteld op basis van gevalideerde klinische criteria of zeer zelden alleen maar kan worden bevestigd door de mening van deskundigen. Nadat je de casus hebt gelezen, wil ik dat je een differentiële diagnose geeft met een lijst met kandidaat-diagnoses, gerangschikt op waarschijnlijkheid, te beginnen met de meest waarschijnlijke kandidaat. Elke kandidaat moet worden gespecificeerd met de ziektenaam. Als de eerste kandidaat bijvoorbeeld het Branchiooculofaciaal syndroom is en de tweede cystische fibrose, geef het dan zo weer:
 
-1. OMIM:113620 - Branchio-oculo-faciaal syndroom
-2. OMIM:219700 - Taaislijmziekte
+1. Branchio-oculofaciaal syndroom
+2. Cystische fibrose
 
 Deze lijst moet zoveel diagnoses bevatten als je redelijk acht.
 
-Je hoeft je redenering niet uit te leggen, je hoeft alleen de diagnoses samen met de OMIM-identifiers op te sommen. Dit is het geval:
+Je hoeft je redenering niet uit te leggen, vermeld alleen de diagnoses.
+Hier is het geval:
 """;
     }
 
